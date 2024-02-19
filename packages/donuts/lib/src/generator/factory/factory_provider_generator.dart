a1dd1893db43e74632dd20eba0d809d982e4c8a7
kw <powerboo.88@gmail.com>
2024-02-19 21:16:07 +0900

    add provider

---
+++ packages/donuts/lib/src/generator/factory/factory_provider_generator.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+import 'package:donuts/src/generator/common/element_checker.dart';
+import 'package:donuts/src/generator/common/names/factory/abstract_interface_factory_name.dart';
+import 'package:donuts/src/generator/common/names/factory/factory_impl_name.dart';
+import 'package:donuts_annotation/donuts_annotation.dart';
+import 'package:source_gen/source_gen.dart';
+
+final _formatter = DartFormatter();
+
+class FactoryImplGenerator extends GeneratorForAnnotation<AggregateRoot> {
+  @override
+  FutureOr<String> generate(LibraryReader library, BuildStep buildStep) {
+    return super.generate(library, buildStep);
+  }
+
+  @override
+  generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) async {
+    return '''
+final factoryProvider = Provider((ref) {
+  return Factory();
+});
+''';
+  }
+}
+
+class FactoryImplGeneratorException implements Exception {
+  late final String message;
+  FactoryImplGeneratorException(final String message) {
+    this.message = "[FactoryImplGeneratorException] $message";
+  }
+  @override
+  String toString() {
+    return message;
+  }
+}
